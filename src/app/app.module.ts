39bd36cccfb818ef5684aabe0298c79affc9a9bd
AxelIvan23 <axelaxel23v@gmail.com>
2020-11-23 17:47:47 -0600

    App6 Web Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReactiveComponent } from './components/reactive/reactive/reactive.component';
+import { HomeComponent } from './components/home/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
